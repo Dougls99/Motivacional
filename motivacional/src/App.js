136752ea43bbde3ecbbbc82368a7aa87faa7dd24
Douglas Pereira <dougls99@outlook.com>
2022-06-03 17:12:07 -0300

    Construindo estruturas react

---
+++ motivacional/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Quotes from './components/Quotes';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello World!!</h1>
+     <Quotes />
+    </div>
+  );
+}
+
+export default App;
---
+++ motivacional/src/components/Quotes.js
@@ -0,0 +1,9 @@
+const Quotes = () => {
+  return (
+    <div id="quote-box">
+      Frases
+    </div>
+  )
+}
+
+export default Quotes;
