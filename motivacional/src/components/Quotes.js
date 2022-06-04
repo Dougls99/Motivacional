9662acacdfb298ca6f0d54145530acf8288fc0ad
Douglas Pereira <dougls99@outlook.com>
2022-06-04 13:13:50 -0300

    concluindo MVP do projeto funcionando

--- motivacional/src/components/Quotes.js
+++ motivacional/src/components/Quotes.js
@@ -4,17 +4,38 @@ import twitterIcon from "../twitter.svg";
 
 const Quotes = () => {
 
- 
+  const [quote, setQuote] = useState("title");
+  const [author, setAuthor] = useState("");
+  
+  useEffect(() => {
+    getQuote()
+  },[]);
 
+  const getQuote = () => {
+    let url = `https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json`;
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        let dataQuotes = data.quotes;
+        let randomNum = Math.floor(Math.random() * dataQuotes.length);
+        let randomQuote = dataQuotes[randomNum];
+        
+        setQuote(randomQuote.quote);
+        setAuthor(randomQuote.author);
+      })
+  }
 
+  const handleClick = () => {
+    getQuote();
+  }
 
   return (
     <div id="quote-box">
       <div id="text">
-        <p>Frase</p>
+        <p>{quote}</p>
       </div>
       <div id="author">
-        <p>Jonas</p>
+        <p>{author}</p>
       </div>
 
       <div id="buttons">
@@ -23,7 +44,7 @@ const Quotes = () => {
             <img src={twitterIcon} alt="" />
           </a>
         </div>
-        <button id="new-quote">Nova Frase</button>
+        <button onClick={handleClick} id="new-quote">Nova Frase</button>
       </div>
     </div>
   )
