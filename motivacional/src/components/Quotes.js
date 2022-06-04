8dfbf7dab85b47197f750fc5cc840be093d9a1cf
Douglas Pereira <dougls99@outlook.com>
2022-06-04 10:40:33 -0300

    alterações no css

--- motivacional/src/components/Quotes.js
+++ motivacional/src/components/Quotes.js
@@ -1,9 +1,26 @@
 const Quotes = () => {
   return (
     <div id="quote-box">
-      Frases
+      <div id="text">
+        <p>Frase</p>
+      </div>
+      <div id="author">
+        <p>Jonas</p>
+      </div>
+
+      <div id="buttons">
+        <div className="social-media">
+          <a href="#" id="twet-quote">
+            <img src="" alt="" />
+          </a>
+          <a href="#" id="tumlr-quote">
+            <img src="" alt="" />
+          </a>
+        </div>
+        <button id="new-quote">Nova Frase</button>
+      </div>
     </div>
   )
 }
 
-export default Quotes;
+export default Quotes
